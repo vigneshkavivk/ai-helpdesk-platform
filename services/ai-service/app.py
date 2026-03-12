bef90feeeb7da8107b61b9e89b94d601eef9c0f0
vigneshkavi <vigneshkavivk@gmail.com>
2026-03-12 11:07:55 +0500

    kubernetes/

--- services/ai-service/app.py
+++ services/ai-service/app.py
@@ -1,22 +1,29 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, render_template
 from model import predict_intent
 
 app = Flask(__name__)
 
+# UI page
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+# AI API
 @app.route("/analyze", methods=["POST"])
 def analyze():
 
-    data = request.json
-    message = data["message"]
+    data = request.get_json()
+    message = data.get("message")
 
     intent = predict_intent(message)
 
-    return jsonify({"intent":intent})
+    return jsonify({"intent": intent})
 
-
+# health check (for Kubernetes)
 @app.route("/health")
 def health():
     return "ok"
 
 
-app.run(host="0.0.0.0",port=5000)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
